bcffc6b565687dd0f60feffc103ba4131d3a6568
Event 42 <event@c2r6s6.42madrid.com>
2022-01-29 20:49:32 +0100

    Add bat enemy to the game

---
+++ Assets/MovBat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovBat : MonoBehaviour
+ {
+ 
+     private float RotateSpeed = 3f;
+     private float Radius = 0.05f;
+ 
+     private Vector2 _centre;
+     private float _angle;
+ 
+     private void Start()
+     {
+         _centre = transform.position;
+     }
+ 
+     private void Update()
+     {
+ 
+         _angle += RotateSpeed * Time.deltaTime;
+ 
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+        transform.Translate(offset.x,offset.y,0);
+    }
+}
